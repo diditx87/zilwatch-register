c12970a019cf457ada7f49fe5751bd2794e3f530
Didit Apriyanto <42115824+diditx87@users.noreply.github.com>
2022-02-06 15:17:41 +0700

    Update send.js

--- send.js
+++ send.js
@@ -7,7 +7,7 @@ const {
     getAddressFromPrivateKey,
 } = require('@zilliqa-js/crypto');
 
-const zilliqa = new Zilliqa('https://ssn.zillet.io/');
+const zilliqa = new Zilliqa('https://seed-zil.shardpool.io');
 const CHAIN_ID = 1;
 const MSG_VERSION = 1;
 const VERSION = bytes.pack(CHAIN_ID, MSG_VERSION);
